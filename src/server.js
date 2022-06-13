182d256d7f893bb00787a58c5f0f8c5eb1b20557
Jessica Fenna <jessicafenna@gmail.com>
2022-06-13 15:35:32 +0100

    user model added

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 require("./db/connection");
 const express = require("express"); 
 const app = express(); 
-const port = 5001; 
+const port = process.env.PORT || 8080; 
 
-app.use();
+// app can now use json method - can convert from and to json data 
+app.use(express.json());
+
+// future proofing: 
+// atm always on port 5001 - but in future if you're hosting, the app will assign you a port 
+app.listen(port, ()=> { 
+    console.log(`listening on port ${port}`)
+})
+
+
