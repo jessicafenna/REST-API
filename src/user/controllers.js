70de1fe28c9000301e17a1c4025b3b7fcba53f14
Jessica Fenna <jessicafenna@gmail.com>
2022-06-16 15:35:40 +0100

    thunderclient req success

--- src/user/controllers.js
+++ src/user/controllers.js
@@ -1,7 +1,7 @@
 const User = require("./model");
 
 // after async() - parameters passed are empty variable names,you can call them ANYTHING - but try to be clear and consistent
-exports.createUser = async (res, req) =>{ 
+exports.createUser = async (req, res) =>{ 
     try{ 
         console.log (req, res);
         res.send({message:"End of controller"})
