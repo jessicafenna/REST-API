a7c9822bff42930fbd44b0af9732ab73765c5b89
Jessica Fenna <jessicafenna@gmail.com>
2022-06-22 16:43:44 +0100

    cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 require("./db/connection");
 const express = require("express"); 
 const app = express(); 
+const cors = require ("cors");
 const port = process.env.PORT || 5001; 
 const userRouter = require("./user/routes");
 
 // app can now use json method - can convert from and to json data 
 app.use(express.json());
+// install cors and use ABOVE router
+app.use(cors());
 app.use(userRouter);
 // future proofing: 
 // atm always on port 5001 - but in future if you're hosting, the app will assign you a port 
--- src/user/controllers.js
+++ src/user/controllers.js
@@ -3,8 +3,11 @@ const User = require("./model");
 // after async() - parameters passed are empty variable names,you can call them ANYTHING - but try to be clear and consistent
 exports.createUser = async (req, res) =>{ 
     try{ 
-        console.log (req, res);
-        res.send({message:"End of controller"})
+        const userObj = {username:req.body.username, 
+            email: req.body.email, 
+            password: req.body.password}
+        const newUser = await User.create(userObj);
+        res.send({newUser});
     } catch (error){ 
         console.log(error);
         res.send(error);
